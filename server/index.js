838e3605d44643805f8d5b788374dacd5f5fe9e1
Akbar Afriansyah <abam.akbar07@gmail.com>
2021-02-23 16:00:34 +0700

    konfigurasi awal

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+// require("dotenv").config();
+
+const app = express();
+const router = require("./src/routes");
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.static(__dirname + "/uploads"));
+app.use("/api/v1", router);
+const port = 5000;
+
+app.listen(port, () => {
+  console.log(`Listening to port ${port}, App Ready !`);
+});
