9c7cdc1ac87d74339bb7b5ca136c6e0d1e62b0c6
Akbar Afriansyah <abam.akbar07@gmail.com>
2021-03-16 16:32:11 +0700

    config heroku

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static(__dirname + "/uploads"));
 app.use("/api/v1", router);
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Listening to port ${port}, App Ready !`);
