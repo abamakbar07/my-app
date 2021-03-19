331cceddb38be901f86f6d34c6c30e5e019131d5
Akbar Afriansyah <abam.akbar07@gmail.com>
2021-03-19 12:58:11 +0700

    ganti bahasa keterangan buku kosong

--- client/src/pages/PrivatePage/Profile/ProfileListBooks.jsx
+++ client/src/pages/PrivatePage/Profile/ProfileListBooks.jsx
@@ -8,46 +8,65 @@ const ProfileListBooks = () => {
    const listBook = state.userBookList
 
    return (
-      <div>
-         <div className="ProfileListbooks pb-5">
-            {loading ? (
-                  <div className="container text-center p-5 m-5">
-                     <Spinner animation="border" role="status"></Spinner>
-                  </div>
-               ) : listBook.length < 1 ? ( 
-                  <div className="container text-center">
-                     Anda belum memilih buku
-                  </div> ) : (
-               <Row>
-                  {listBook.map((book) => (
-                     <Col sm="4">
-                        
-                           <a href={book.idTransaction.paymentStatus === "Approve" ? "http://localhost:5000/books/"+book.idBook.bookFile : ""}>
-                              {/* <Card onClick={() => getbook(bookList.id)} className=" bg-transparent border-0"> */}
-                              <Card className=" bg-transparent border-0">
-                                 <Card.Img variant="top" src={"http://localhost:5000/books/"+book.idBook.bookThumbnail} style={{width: "10vw", height: "30vh"}} />
-                                 <Card.Body className="text-left p-0 pt-2">
-                                    <Card.Title className="ListBooks-title" >
-                                       {book.idBook.title}
-                                    </Card.Title>
-                                    <Card.Text className="text-muted">
-                                       {book.idBook.author}
-                                    </Card.Text>
-                                    <Button className={book.idTransaction.paymentStatus === "Pending" ? "disabled" : ""}>
-                                       {book.idTransaction.paymentStatus === "Pending" ? "Transaction on process" : "Download" }
-                                    </Button>
-                                 </Card.Body>
-                              </Card>
-                           </a>
-
-                     </Col>
-                     )
-                     )}
-                  </Row>
-               )} 
-         </div>
-      </div>
-   )
+     <div>
+       <div className="ProfileListbooks pb-5">
+         {loading ? (
+           <div className="container text-center p-5 m-5">
+             <Spinner animation="border" role="status"></Spinner>
+           </div>
+         ) : listBook.length < 1 ? (
+           <div className="container text-center">
+             You don't have a book to read yet. Buy now!
+           </div>
+         ) : (
+           <Row>
+             {listBook.map((book) => (
+               <Col sm="4">
+                 <a
+                   href={
+                     book.idTransaction.paymentStatus === "Approve"
+                       ? "http://localhost:5000/books/" + book.idBook.bookFile
+                       : ""
+                   }
+                 >
+                   {/* <Card onClick={() => getbook(bookList.id)} className=" bg-transparent border-0"> */}
+                   <Card className=" bg-transparent border-0">
+                     <Card.Img
+                       variant="top"
+                       src={
+                         "http://localhost:5000/books/" +
+                         book.idBook.bookThumbnail
+                       }
+                       style={{ width: "10vw", height: "30vh" }}
+                     />
+                     <Card.Body className="text-left p-0 pt-2">
+                       <Card.Title className="ListBooks-title">
+                         {book.idBook.title}
+                       </Card.Title>
+                       <Card.Text className="text-muted">
+                         {book.idBook.author}
+                       </Card.Text>
+                       <Button
+                         className={
+                           book.idTransaction.paymentStatus === "Pending"
+                             ? "disabled"
+                             : ""
+                         }
+                       >
+                         {book.idTransaction.paymentStatus === "Pending"
+                           ? "Transaction on process"
+                           : "Download"}
+                       </Button>
+                     </Card.Body>
+                   </Card>
+                 </a>
+               </Col>
+             ))}
+           </Row>
+         )}
+       </div>
+     </div>
+   );
 }
 
 export default ProfileListBooks
