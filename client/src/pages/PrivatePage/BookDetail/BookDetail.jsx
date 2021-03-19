8b4f288bd2d798fb49704ec89ede4e3871656d42
Akbar Afriansyah <abam.akbar07@gmail.com>
2021-03-19 13:43:28 +0700

    Resset bookDetail navigation

--- client/src/pages/PrivatePage/BookDetail/BookDetail.jsx
+++ client/src/pages/PrivatePage/BookDetail/BookDetail.jsx
@@ -17,16 +17,6 @@ const BookDetail = () => {
    const [booksResult, setBooksResult] = useState([])
    const [addCart, setAddCart] = useState(true)
 
-   // const [index, setIndex] = useState(id);
-
-   // const handleSelect = (selectedIndex, e) => {
-   //    setIndex(selectedIndex);
-   // };
-
-   // const getIndex = () => {
-   //    setIndex(parseInt(id)-1)
-   // }
-
    const getAddCart = () => {
       setLoading(true)
       let i
@@ -43,7 +33,6 @@ const BookDetail = () => {
          const books = await API.get("/books");
          await setBookResult(book.data.data.book);
          await setBooksResult(books.data.data.books);
-         // getIndex()
          
          setLoading(false);
       } catch (error) {
@@ -78,111 +67,99 @@ const BookDetail = () => {
          </div>
        ) : (
          <div className="BookDetail-container">
-           {/* <Carousel
-             fade
-             activeIndex={index}
-             onSelect={handleSelect}
-             indicators={false}
-             style={{ zIndex: "1", padding: "0vw 5vw" }}
-           > */}
-             {/* {booksResult.map((book) => ( */}
-               {/* <Carousel.Item data-interval={false}> */}
-                 <Card body className="border-0 bg-transparent">
-                   <div className="row">
-                     <div className="col-md-4">
-                       <ListGroup>
-                         <img
-                           alt=""
-                           src={
-                             "http://localhost:5000/books/" + bookResult.bookThumbnail
-                           }
-                           style={{ width: "100%" }}
-                         />
-                       </ListGroup>
-                     </div>
+            <Card body className="border-0 bg-transparent">
+               <div className="row">
+               <div className="col-md-4">
+                  <ListGroup>
+                     <img
+                     alt=""
+                     src={
+                        "http://localhost:5000/books/" + bookResult.bookThumbnail
+                     }
+                     style={{ width: "100%" }}
+                     />
+                  </ListGroup>
+               </div>
 
-                     <div className="col-md-8">
-                       <ListGroup horizontal>
-                         <ListGroup.Item className="text-left border-0 bg-transparent">
-                           <p className="BookDetail-title m-0 font-weight-bold">
-                             {bookResult.title}
-                           </p>
-                           <small className="text-muted">{bookResult.author}</small>
-                         </ListGroup.Item>
-                       </ListGroup>
-                       <ListGroup horizontal>
-                         <ListGroup.Item className="text-left border-0 bg-transparent mt-3">
-                           <p className="m-0 font-weight-bold">
-                             Publication date
-                           </p>
-                           <small className="text-muted">
-                             {bookResult.publicationDate}
-                           </small>
-                         </ListGroup.Item>
-                       </ListGroup>
-                       <ListGroup horizontal>
-                         <ListGroup.Item className="text-left border-0 bg-transparent">
-                           <p className="m-0 font-weight-bold">Pages</p>
-                           <small className="text-muted">{bookResult.pages}</small>
-                         </ListGroup.Item>
-                       </ListGroup>
-                       <ListGroup horizontal>
-                         <ListGroup.Item className="text-left border-0 bg-transparent">
-                           <p className="m-0 font-weight-bold text-danger">
-                             ISBN
-                           </p>
-                           <small className="text-muted">{bookResult.isbn}</small>
-                         </ListGroup.Item>
-                       </ListGroup>
-                       <ListGroup horizontal>
-                         <ListGroup.Item className="text-left border-0 bg-transparent">
-                           <p className="m-0 font-weight-bold text-success">
-                             Price
-                           </p>
-                           <small className="text-muted">
-                             {"Rp. " + bookResult.price}
-                           </small>
-                         </ListGroup.Item>
-                       </ListGroup>
-                     </div>
-                   </div>
+               <div className="col-md-8">
+                  <ListGroup horizontal>
+                     <ListGroup.Item className="text-left border-0 bg-transparent">
+                     <p className="BookDetail-title m-0 font-weight-bold">
+                        {bookResult.title}
+                     </p>
+                     <small className="text-muted">{bookResult.author}</small>
+                     </ListGroup.Item>
+                  </ListGroup>
+                  <ListGroup horizontal>
+                     <ListGroup.Item className="text-left border-0 bg-transparent mt-3">
+                     <p className="m-0 font-weight-bold">
+                        Publication date
+                     </p>
+                     <small className="text-muted">
+                        {bookResult.publicationDate}
+                     </small>
+                     </ListGroup.Item>
+                  </ListGroup>
+                  <ListGroup horizontal>
+                     <ListGroup.Item className="text-left border-0 bg-transparent">
+                     <p className="m-0 font-weight-bold">Pages</p>
+                     <small className="text-muted">{bookResult.pages}</small>
+                     </ListGroup.Item>
+                  </ListGroup>
+                  <ListGroup horizontal>
+                     <ListGroup.Item className="text-left border-0 bg-transparent">
+                     <p className="m-0 font-weight-bold text-danger">
+                        ISBN
+                     </p>
+                     <small className="text-muted">{bookResult.isbn}</small>
+                     </ListGroup.Item>
+                  </ListGroup>
+                  <ListGroup horizontal>
+                     <ListGroup.Item className="text-left border-0 bg-transparent">
+                     <p className="m-0 font-weight-bold text-success">
+                        Price
+                     </p>
+                     <small className="text-muted">
+                        {"Rp. " + bookResult.price}
+                     </small>
+                     </ListGroup.Item>
+                  </ListGroup>
+               </div>
+               </div>
 
-                   <div className="row mt-5 mb-5">
-                     <div className="col-sm-12">
-                       <p className="BookDetail-aboutTitle text-left">
-                         About This Book
-                       </p>
-                       <p className="BookDetail-aboutSub text-left">
-                         {bookResult.about}
-                       </p>
-                     </div>
-                   </div>
+               <div className="row mt-5 mb-5">
+               <div className="col-sm-12">
+                  <p className="BookDetail-aboutTitle text-left">
+                     About This Book
+                  </p>
+                  <p className="BookDetail-aboutSub text-left">
+                     {bookResult.about}
+                  </p>
+               </div>
+               </div>
 
-                   <div className={loading ? "d-false" : "row"}>
-                     <div className="col-sm-12 text-right">
-                       <Link to="/cart" className={addCart ? "" : "d-none"}>
-                         <button
-                           onClick={() => addProductToCart(bookResult.id)}
-                           className="btn globalButtonNoRound m-1"
-                         >
-                           Add To Cart
-                           <img alt="" className="ml-2" src={iconBookmark} />
-                         </button>
-                       </Link>
-                       <Link className={addCart ? "d-none" : ""}>
-                         <button
-                           className="btn btn-light m-1"
-                           style={{ background: "rgba(205, 205, 205, 0.7)" }}
-                         >
-                           Book Added
-                         </button>
-                       </Link>
-                     </div>
-                   </div>
-                 </Card>
-               {/* </Carousel.Item> */}
-            {/* ))} */}
-           {/* </Carousel> */}
+               <div className={loading ? "d-false" : "row"}>
+               <div className="col-sm-12 text-right">
+                  <Link to="/cart" className={addCart ? "" : "d-none"}>
+                     <button
+                     onClick={() => addProductToCart(bookResult.id)}
+                     className="btn globalButtonNoRound m-1"
+                     >
+                     Add To Cart
+                     <img alt="" className="ml-2" src={iconBookmark} />
+                     </button>
+                  </Link>
+                  <Link className={addCart ? "d-none" : ""}>
+                     <button
+                     className="btn btn-light m-1"
+                     style={{ background: "rgba(205, 205, 205, 0.7)" }}
+                     >
+                     Book Added
+                     </button>
+                  </Link>
+               </div>
+               </div>
+            </Card>
          </div>
        )}
      </div>
